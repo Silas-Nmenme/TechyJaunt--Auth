8ff9be78e17ba3fadcb49d9d2bead93f3cbb8329
Your Name <you@example.com>
2025-08-02 20:39:13 +0100

    debug

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const morgan = require("morgan")
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
 
 // Importing necessary modules
-const connectDB = require("./src/config/db")
-const userRouter = require("./src/routes/user.routes")
-const carRouter = require("./src/routes/car.routes")
-const paymentRouter = require("./src/routes/payment.routes.js")
-const path = require("path")
-const sendSms = require("./src/utils/sendSms.js");
+const connectDB = require("./src/config/db");
+const userRouter = require("./src/routes/user.routes");
+const carRouter = require("./src/routes/car.routes");
+const paymentRouter = require("./src/routes/payment.routes.js");
 
-
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4500
+const PORT = process.env.PORT || 4500;
 
 // Middleware
-app.use(express.json())
-app.use(morgan("dev"))
-
+app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes
 app.get("/", (req, res) => res.send("Welcome To Silas Rental Services"));
-app.use("/api/users", userRouter)
-app.use("/api/cars", carRouter)
-app.use("/api/payment", paymentRouter)
+app.use("/api/users", userRouter);
+app.use("/api/cars", carRouter);
+app.use("/api/payment", paymentRouter);
 
-//Start the server
+// Start the server
 app.listen(PORT, () => {
-  connectDB()
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
