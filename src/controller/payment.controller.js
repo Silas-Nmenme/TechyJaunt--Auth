756ae67f37af7260282cc430c9871e81c8f9af21
Your Name <you@example.com>
2025-09-11 18:15:18 +0100

    `Update FLW redirect URL in makePayment function`

--- src/controller/payment.controller.js
+++ src/controller/payment.controller.js
@@ -61,7 +61,7 @@ exports.makePayment = async (req, res) => {
       currency: 'NGN',
       redirect_url:
         process.env.FLW_REDIRECT_URL ||
-        'https://silascarrentals.netlify.app/payment-callback.html',
+        'https://techyjaunt-auth-go43.onrender.com/api/payment/flutterwave/callback',
       customer: {
         email,
         phonenumber: phone_number,
